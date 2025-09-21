d7d6d366b71d3b31e211e696d59a08edc7994453
xiaoma0515 <maxiao2019@centrum.cz>
2025-09-21 23:09:39 +0200

    notion_habit.py

--- notion_habit.py
+++ notion_habit.py
@@ -2,7 +2,7 @@ import os
 import requests
 from datetime import date
 
-# 从环境变量读取 Notion Token 和 Database ID（安全做法）
+# 从 GitHub Secrets 获取 Token 和 Database ID
 NOTION_TOKEN = os.environ['NOTION_TOKEN']
 DATABASE_ID = os.environ['NOTION_DATABASE_ID']
 
@@ -12,21 +12,22 @@ headers = {
     "Notion-Version": "2022-06-28"
 }
 
-today = date.today().isoformat()
+today = date.today().isoformat()  # 例如 '2025-09-21'
 
-# 如果你有多个习惯要关联，需要先获取 Habit 的 page_id
-# 假设你要关联两个习惯，这里手动填入 Habit 页面 ID
-habit_ids = [
-    "习惯ID1",  # 替换成你习惯页面的ID
-    "习惯ID2"   # 可以加更多
+# Gym 列关联的页面 ID（需要提前获取 Gym 表中你要关联的页面 ID）
+gym_page_ids = [
+    "gym-page-id-1",  # 替换成 Gym 表中某个页面的 ID
 ]
 
 data = {
     "parent": {"database_id": DATABASE_ID},
     "properties": {
+        "Day": {"title": [{"text": {"content": "Today"}}]},
         "Date": {"date": {"start": today}},
-        "Habit": {"relation": [{"id": hid} for hid in habit_ids]},
-        "Done": {"checkbox": False}
+        "Gym": {"relation": [{"id": gid} for gid in gym_page_ids]},
+        "Workout": {"checkbox": False},
+        "Checkbox 1": {"checkbox": False},
+        "Checkbox 2": {"checkbox": False}
     }
 }
 
